0a859e3be6a6fd11a9c6c235dd8951fecb16c1fc
Francisco Funes <ffunes@hexacta.com>
2022-01-18 14:50:47 -0300

    feat: 70. Restricting access with interfaces

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,6 +1,15 @@
 import { User } from './User';
 import { Company } from './Company';
 
+// Instruction to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -11,8 +20,7 @@ export class CustomMap {
     });
   }
 
-  // bad code example
-  addMarker(mappable: User | Company): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
@@ -21,14 +29,4 @@ export class CustomMap {
       },
     });
   }
-
-  // addCompanyMarker(company: Company): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: {
-  //       lat: company.location.lat,
-  //       lng: company.location.lng,
-  //     },
-  //   });
-  // }
 }
