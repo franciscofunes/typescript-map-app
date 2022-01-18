362730acf638aa98cddcb400be4be4f23bda94a0
Francisco Funes <ffunes@hexacta.com>
2022-01-18 15:24:55 -0300

    feat: 72. showing popup window

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -18,12 +18,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: '<h3> Latitude: ' + mappable.location.lat + '</h3>',
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
